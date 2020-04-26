6e44f3921dc08d650f16f3c380a57fb5dde44a15
kennja05 <jacobkenny05@gmail.com>
2020-04-26 15:48:00 -0400

    added index and show methods for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,12 @@
 class ListsController < ApplicationController
+
+    def index
+        lists = List.all 
+        render json: lists
+    end 
+
+    def show
+        list = List.find(params['id'])
+        render json: list, include: :categories
+    end 
 end
