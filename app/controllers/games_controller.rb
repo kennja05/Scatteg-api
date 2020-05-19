57dde38e12998c483d03db619339fd0eaf39eab3
kennja05 <jacobkenny05@gmail.com>
2020-05-19 17:27:11 -0400

    updated games controller with index and create actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,13 @@
 class GamesController < ApplicationController
+
+    def index
+        game = Game.all 
+        render json: game, include: :rounds
+    end 
+
+    def create
+        byebug
+    end
+
+
 end
