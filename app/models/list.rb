f055b5721d6364d2a8f83600a2bb717fa56ed333
kennja05 <jacobkenny05@gmail.com>
2020-05-20 11:33:33 -0400

    created roundlists and associations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
     has_many :categories
+    has_many :roundlists
+    has_many :rounds, through: :roundlists #may need to reconsider pluralization/punctuation of roundlists
 end
