f67697f97a5d4a39ab14a9e883e58df2c86bdfc3
kennja05 <jacobkenny05@gmail.com>
2020-05-22 18:07:54 -0400

    added controller methods for rounds

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,2 +1,19 @@
 class RoundsController < ApplicationController
+
+    def index
+        rounds = Round.all 
+        render json: rounds
+    end
+
+
+    def create
+        byebug
+    end
+
+    private
+
+    def round_params
+        params.require(:round).permit(:game_id, :qOne, :qTwo, :qThree, :qFour, :qFive, :qSix, :qSeven, :qEight, :qNine, :qTen, :qEleven, :qTwelve, :score)
+    end
+
 end
